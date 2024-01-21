f0d1c9ecf1544bfbf337938b29818c13ea90340c
The11Doctor90 <matteo.ferreri@live.it>
2024-01-21 17:30:22 +0100

    feat: SOM-10 divide frame in header body and footer

--- SomniiaMonitor/View/somniia_ui.py
+++ SomniiaMonitor/View/somniia_ui.py
@@ -1,14 +1,27 @@
 #  Copyright (c) Matteo Ferreri 2024.
-
+import kivy
 from kivy.app import App
-from kivy.properties import StringProperty
-from SomniiaMonitor.Business.lang_observer import Lang
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
 
-tr = Lang("it")
+kivy.require('2.3.0')
+
+
+class WelcomeScreen(Screen):
+    def __init__(self, **kwargs):
+        super(WelcomeScreen, self).__init__(**kwargs)
+
+    def click_button(self):
+        app = App.get_running_app()
+        sm = app.root.ids.sm
+        sm.current = 'second_screen'
+
+
+class SecondScreen(Screen):
+    pass
 
 
 class SomniiaApp(App):
-    lang = StringProperty('it')
-
-    def on_lang(self, instance, lang):
-        tr.switch_lang(lang)
+    def build(self):
+        self.title = 'screen with header and footer'
+        return Builder.load_file('SomniiaMonitor/View/somniia.kv')
