adeb869c54b829be71e3821cb2d8130290fc59d1
The11Doctor90 <matteo.ferreri@live.it>
2024-01-24 20:23:36 +0100

    feat: SOM-11 BLE device selection

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 #  Copyright (c) Matteo Ferreri 2024.
-import platform
-import asyncio
-
-from kivy.app import async_runTouchApp
-from kivy.uix.label import Label
 
 import SomniiaMonitor.View.somniia_ui as fr
 
