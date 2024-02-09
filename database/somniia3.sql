dd8a0497d44ecfa5ed93b7fa3e812c0ed90c0bcd
matteo_ferreri <matteo.ferreri@webscience.it>
2024-02-09 17:54:23 +0100

    feat: SOM-19 create business model

--- database/somniia3.sql
+++ database/somniia3.sql
@@ -26,14 +26,10 @@ CREATE TABLE IF NOT EXISTS users (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT ,
     name TEXT NOT NULL,
     surname TEXT NOT NULL,
-    tax_id TEXT NOT NULL ,
+    tax_id TEXT UNIQUE NOT NULL ,
     birth_date TEXT NOT NULL,
     gender TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    fk_doctor_id TEXT,
-    fk_contact_id INTEGER,
-    FOREIGN KEY (fk_doctor_id) REFERENCES doctors(doctor_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (fk_contact_id) references contacts(contact_id) ON UPDATE CASCADE ON DELETE CASCADE
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Contacts Table
@@ -47,18 +43,23 @@ CREATE TABLE IF NOT EXISTS contacts(
     province TEXT NOT NULL,
     zip TEXT NOT NULL ,
     country TEXT NOT NULL,
-    fk_user_id INTEGER,
-    FOREIGN KEY (fk_user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE
+    tax_id TEXT,
+    FOREIGN KEY (tax_id) REFERENCES users(tax_id) ON DELETE CASCADE
 );
 
 -- Doctors Table
 CREATE TABLE IF NOT EXISTS doctors (
-    doctor_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    doctor_tax_id TEXT,
     register_code TEXT UNIQUE NOT NULL,
-    fk_user_id INTEGER,
-    id_supervisor INTEGER,
-    FOREIGN KEY (fk_user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (id_supervisor) REFERENCES doctors(doctor_id) ON DELETE SET NULL
+    id_supervisor TEXT,
+    FOREIGN KEY (doctor_tax_id) REFERENCES users(tax_id) ON DELETE CASCADE,
+    FOREIGN KEY (id_supervisor) REFERENCES doctors(doctor_tax_id) ON DELETE SET NULL
+);
+
+-- Sleeper Table
+CREATE TABLE IF NOT EXISTS sleepers (
+    sleeper_tax_id TEXT,
+    FOREIGN KEY (sleeper_tax_id) REFERENCES users(tax_id) ON DELETE CASCADE
 );
 
 -- Mask Table
@@ -75,26 +76,12 @@ CREATE TABLE IF NOT EXISTS analyses (
     start DATETIME DEFAULT CURRENT_TIMESTAMP,
     stop DATETIME DEFAULT CURRENT_TIMESTAMP,
     code TEXT UNIQUE NOT NULL ,
-    fk_user_id INTEGER,
-    fk_doctor_id INTEGER,
-    fk_mask_id INTEGER,
-    fk_ekg_signal_id INTEGER,
-    fk_ekg_parameter_id INTEGER,
-    fk_eeg_signal_id INTEGER,
-    fk_ppg_param_id INTEGER,
-    fk_inertial_param_id INTEGER,
-    fk_sleep_stage_id INTEGER,
-    fk_temperature_id INTEGER,
-    FOREIGN KEY (fk_user_id) REFERENCES users(user_id) ON DELETE CASCADE ,
-    FOREIGN KEY (fk_doctor_id) REFERENCES doctors(doctor_id) ON DELETE CASCADE,
-    FOREIGN KEY (fk_mask_id) REFERENCES masks(mask_id) ON DELETE CASCADE,
-    FOREIGN KEY (fk_ekg_signal_id) REFERENCES ekg_signals(ekg_signal_id) ON DELETE CASCADE,
-    FOREIGN KEY (fk_ekg_parameter_id) REFERENCES ekg_parameters(ekg_parameter_id) ON DELETE CASCADE,
-    FOREIGN KEY (fk_eeg_signal_id) REFERENCES eeg_signals(eeg_signal_id) ON DELETE CASCADE,
-    FOREIGN KEY (fk_ppg_param_id) REFERENCES ppg_params(ppg_param_id) ON DELETE CASCADE,
-    FOREIGN KEY (fk_inertial_param_id) REFERENCES inertial_params(inertial_param_id) ON DELETE CASCADE,
-    FOREIGN KEY (fk_sleep_stage_id) REFERENCES sleep_stages(sleep_stage_id) ON DELETE CASCADE,
-    FOREIGN KEY (fk_temperature_id) REFERENCES temperatures(temperature_id) ON DELETE CASCADE
+    sleeper_tax_id TEXT,
+    doctor_tax_id TEXT,
+    mask_mac_addr TEXT,
+    FOREIGN KEY (sleeper_tax_id) REFERENCES sleepers(sleeper_tax_id) ON DELETE SET NULL,
+    FOREIGN KEY (doctor_tax_id) REFERENCES doctors(doctor_tax_id) ON DELETE SET NULL,
+    FOREIGN KEY (mask_mac_addr) REFERENCES masks(mac_addr) ON DELETE SET NULL
 );
 
 -- EKG Signal Table
@@ -102,8 +89,8 @@ CREATE TABLE IF NOT EXISTS ekg_signals (
     ekg_signal_id INTEGER PRIMARY KEY AUTOINCREMENT ,
     time INTEGER NOT NULL ,
     ekg_signal INTEGER NOT NULL,
-    fk_analysis_code TEXT,
-    FOREIGN KEY (fk_analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
+    analysis_code TEXT,
+    FOREIGN KEY (analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
 );
 
 -- EKG Parameter Table
@@ -113,8 +100,8 @@ CREATE TABLE IF NOT EXISTS ekg_parameters (
     hr INTEGER NOT NULL ,
     hrv INTEGER NOT NULL ,
     rr_interval INTEGER NOT NULL ,
-    fk_analysis_code TEXT,
-    FOREIGN KEY (fk_analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
+    analysis_code TEXT,
+    FOREIGN KEY (analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
 );
 
 -- EEG Signal Table
@@ -124,8 +111,8 @@ CREATE TABLE IF NOT EXISTS eeg_signals (
     channel_1 INTEGER NOT NULL,
     channel_2 INTEGER NOT NULL,
     channel_3 INTEGER NOT NULL,
-    fk_analysis_code TEXT,
-    FOREIGN KEY (fk_analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
+    analysis_code TEXT,
+    FOREIGN KEY (analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
 );
 
 -- PPG Parameter Table
@@ -136,8 +123,8 @@ CREATE TABLE IF NOT EXISTS ppg_params (
     spo2 INTEGER NOT NULL ,
     pi REAL NOT NULL ,
     br INTEGER NOT NULL ,
-    fk_analysis_code TEXT,
-    FOREIGN KEY (fk_analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
+    analysis_code TEXT,
+    FOREIGN KEY (analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
 );
 
 -- Inertial Parameter Table
@@ -148,8 +135,8 @@ CREATE TABLE IF NOT EXISTS inertial_params (
     roll REAL NOT NULL,
     pitch REAL NOT NULL,
     yaw REAL NOT NULL,
-    fk_analysis_code TEXT,
-    FOREIGN KEY (fk_analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
+    analysis_code TEXT,
+    FOREIGN KEY (analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
 );
 
 --  Sleep Stage Table
@@ -157,8 +144,8 @@ CREATE TABLE IF NOT EXISTS sleep_stages (
     sleep_stage_id INTEGER PRIMARY KEY AUTOINCREMENT ,
     time INTEGER NOT NULL ,
     stage TEXT NOT NULL,
-    fk_analysis_code TEXT,
-    FOREIGN KEY (fk_analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
+    analysis_code TEXT,
+    FOREIGN KEY (analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
 );
 
 -- Temperature Table
@@ -166,6 +153,6 @@ CREATE TABLE IF NOT EXISTS temperatures (
     temperature_id INTEGER PRIMARY KEY AUTOINCREMENT ,
     time INTEGER NOT NULL,
     value REAL NOT NULL,
-    fk_analysis_code TEXT,
-    FOREIGN KEY (fk_analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
+    analysis_code TEXT,
+    FOREIGN KEY (analysis_code) REFERENCES analyses(code) ON DELETE CASCADE
 );
