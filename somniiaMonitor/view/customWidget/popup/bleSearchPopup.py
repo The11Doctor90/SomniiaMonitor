3819ee8d1f549df32a412a63357de8fd81aa9dc1
fadymena <fadyadel.mena@studenti.unisalento.it>
2024-02-11 15:39:15 +0100

    feat: errore handling in homepage.kv, analysis.py, etc

--- somniiaMonitor/view/customWidget/popup/bleSearchPopup.py
+++ somniiaMonitor/view/customWidget/popup/bleSearchPopup.py
@@ -1,11 +1,11 @@
 #  Copyright (c) Matteo Ferreri 2024.
 
 from kivy.properties import ObjectProperty
-from kivy.uix.popup import Popup
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 
 import somniiaMonitor.business.maskDataReader.bleReader as ble
-from somniiaMonitor.view.customWidget.popup.warningPopup import WarningPopup
 
 
 class BleSearchPopup(Popup):
@@ -25,10 +25,12 @@ class BleSearchPopup(Popup):
                 self.btn = Button(text=device_info.name)
                 self.foundedBleList.add_widget(self.btn)
         except Exception as e:
-            self.warning = WarningPopup.get_instance()
-            self.warning.add_label_text("ciaoco")
-            self.foundedBleList.add_widget(self.warning)
-            # self.warning = WarningPopup()
+            Popup(title='Warning', content=Label(text='No bluetooth adapters found'), size_hint=(None, None),
+                  size=(300, 200)).open()
+            # self.warning = WarningPopup.get_instance()
             # self.warning.add_label_text("ciaoco")
             # self.foundedBleList.add_widget(self.warning)
 
+            # self.warning = WarningPopup()
+            # self.warning.add_label_text("ciaoco")
+            # self.foundedBleList.add_widget(self.warning)
