7f8e5b803ac0f3edcd6c8a1865f2fab054bc7e1b
fadymena <fadyadel.mena@studenti.unisalento.it>
2024-02-11 20:44:38 +0100

    feat: generate mock data instead of fetching from BLE device

--- somniiaMonitor/business/dataFetchingFromDevice.py
+++ somniiaMonitor/business/dataFetchingFromDevice.py
@@ -5,12 +5,18 @@ from somniiaMonitor.business.maskDataReader.inertialReader import InertialReader
 from somniiaMonitor.business.maskDataReader.ppgReader import PpgReader
 from somniiaMonitor.business.maskDataReader.stagingReader import StagingReader
 from somniiaMonitor.business.maskDataReader.temperatureReader import TemperatureReader
+
+from somniiaMonitor.business.model.eeg_signal_business import EegSignalBusiness
+from somniiaMonitor.business.model.ekg_parameter_business import EkgParameterBusiness
+from somniiaMonitor.business.model.inertial_parameter_business import InertialParameterBusiness
+from somniiaMonitor.business.model.ppg_parameter_business import PpgParameterBusiness
+from somniiaMonitor.business.model.sleep_stage_business import SleepStageBusiness
+from somniiaMonitor.business.model.temperature_business import TemperatureBusiness
 import bleak
 import numpy as np
 
 
 class DataFetchingFromDevice:
-    istance = None
     eegReader: EegReader
     ekgReader: EkgReader
     inertialReader: InertialReader
@@ -18,6 +24,13 @@ class DataFetchingFromDevice:
     stagingReader: StagingReader
     temperatureReader: TemperatureReader
     client: bleak.BleakClient
+
+    eegSignalBusiness: EegSignalBusiness
+    ekgParameterBusiness: EkgParameterBusiness
+    inertialParameterBusiness: InertialParameterBusiness
+    ppgParameterBusiness: PpgParameterBusiness
+    stagingParameterBusiness: SleepStageBusiness
+    temperatureParameterBusiness: TemperatureBusiness
 
     def __init__(self, mac: str):
         """init the class responsible for fetching all data from ble device"""
@@ -29,29 +42,49 @@ class DataFetchingFromDevice:
         self.stagingReader = StagingReader(self.client)
         self.temperatureReader = TemperatureReader(self.client)
 
-    def get_eeg_data(self):
-        """ask ble device to send data"""
-        return self.eegReader.read()
+        self.eegSignalBusiness = EegSignalBusiness.get_instance()
 
-    def get_ekg_data(self):
+    def get_eeg_data(self, analysis_id: int):
         """ask ble device to send data"""
-        return self.ekgReader.read()
+        data = self.eegReader.read()
+        data.set_analysis_id(analysis_id)
+        self.eegSignalBusiness.save_eeg_signal(data)
+        return data
 
-    def get_inertial_data(self):
+    def get_ekg_data(self, analysis_id: int):
         """ask ble device to send data"""
-        return self.inertialReader.read()
+        data = self.ekgReader.read()
+        data.set_analysis_id(analysis_id)
+        self.ekgParameterBusiness.save_ekg_parameter(data)
+        return data
 
-    def get_ppg_data(self):
+    def get_inertial_data(self, analysis_id: int):
         """ask ble device to send data"""
-        return self.ppgReader.read()
+        data = self.inertialReader.read()
+        data.set_analysis_id(analysis_id)
+        self.inertialParameterBusiness.save_inertial_parameter(data)
+        return data
 
-    def get_staging_data(self):
+    def get_ppg_data(self, analysis_id: int):
         """ask ble device to send data"""
-        return self.stagingReader.read()
+        data = self.ppgReader.read()
+        data.set_analysis_id(analysis_id)
+        self.ppgParameterBusiness.save_ppg_parameter(data)
+        return data
 
-    def get_temperature_data(self):
+    def get_staging_data(self, analysis_id: int):
         """ask ble device to send data"""
-        return self.temperatureReader.read()
+        data = self.stagingReader.read()
+        data.set_analysis_id(analysis_id)
+        self.stagingParameterBusiness.save_sleep_stage(data)
+        return data
+
+    def get_temperature_data(self, analysis_id: int):
+        """ask ble device to send data"""
+        data = self.temperatureReader.read()
+        data.set_analysis_id(analysis_id)
+        self.temperatureParameterBusiness.save_temperature(data)
+        return data
 
 # def convert_into_array(string: str):
 #     """Converte la stringa in ingresso in un array Numpy"""
