6f567ae82cc5a561d23af8f44c1dcbeda1b5748a
The11Doctor90 <matteo.ferreri@live.it>
2024-02-11 16:24:14 +0100

    feat: SOM-21 salve mask in db

--- somniiaMonitor/view/customWidget/popup/bleSearchPopup.py
+++ somniiaMonitor/view/customWidget/popup/bleSearchPopup.py
@@ -6,11 +6,15 @@ from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 
 import somniiaMonitor.business.maskDataReader.bleReader as ble
+from somniiaMonitor.business.model.mask_business import MaskBusiness
+from somniiaMonitor.model.mask import Mask
 
 
 class BleSearchPopup(Popup):
     bleSearch = ObjectProperty(None)
     foundedBleList = ObjectProperty(None)
+
+    mask_business: MaskBusiness = MaskBusiness.get_instance()
 
     # bleList = ObjectProperty(None)
     def __init__(self, **kwargs):
@@ -19,10 +23,13 @@ class BleSearchPopup(Popup):
     def search(self):
         try:
             devices = ble.find_all_device()
+            mask = Mask()
             self.h = self.height = 0.9
             for i, device_info in enumerate(devices):
+                mask.set_mac_addr(device_info.address)
+                mask.set_name(device_info.name)
                 self.h = self.h + self.height * 0.1
-                self.btn = Button(text=device_info.name)
+                self.btn = Button(text=device_info.name, on_release=self.mask_business.save_mask(mask))
                 self.foundedBleList.add_widget(self.btn)
         except Exception as e:
             Popup(title='Warning', content=Label(text='No bluetooth adapters found'), size_hint=(None, None),
